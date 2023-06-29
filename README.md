735526dc7d49ca5e3cd80d3580fc019073117f2a
MakMuftic <makmuftic@chainsafe.io>
2023-06-29 15:04:54 +0200

    repo setup

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# liquidity-executions
+# mainnet-multisig-executions
+This repository is used to keep track of executing function calls on `Sygma` mainnet multisig.
+The idea is to have a file describing a set of functions that we want to execute on a specific chain for mainnet environment. This way, when opening a new PR, we can describe required admin calls across all supported domains (networks).
+
+The flow is described below:
+ - decide what function calls need to be executed on the mainnet multisig
+ - create a PR and populate it with required data to be review by the `Sygma` team
+ - add proposal context in PR description
+ - after agreeing that everything is as it should be, execute the calls on mainnet
+ - after execution add tx hash to file(s) and merge the PR to mark that everything went well and is working as intended on the mainnet
+
+### Create new proposal
+
+The script takes the following command-line arguments:
+
+```bash
+./proposal.sh <root_folder> <directory1> <directory2> ... <num_migrations>
+```
+
+- `<root_folder>` : Specify the root folder representing the supported chain architectures. Valid values are evm and substrate.
+- `<chain1> <chain2>` : Specify one or more directories representing the chain names where the migration files will be created.
+- `<num_migrations>` (optional): Specify the number of migrations to be created per chain name. If omitted, a single migration will be created.
---
+++ proposal.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+root_folder="$1"
+template_file=""
+extension=""
+timestamp=$(date +%Y_%m_%d)
+
+# Check if the root folder is valid
+if [ "$root_folder" != "evm" ] && [ "$root_folder" != "substrate" ]; then
+  echo "Invalid root folder specified. Please use 'evm' or 'substrate' as the root folder representing supported chain architectures."
+  exit 1
+fi
+
+# Determine the template file based on the root folder
+if [ "$root_folder" == "evm" ]; then
+  template_file="proposal_evm_template.json"
+  extension="json"
+else
+  template_file="proposal_substrate_template.json"
+  extension="json"
+fi
+
+# Check if the template file exists
+if [ ! -f "$template_file" ]; then
+  echo "Template file not found: $template_file"
+  exit 1
+fi
+
+# Check if there are any directory arguments provided
+if [ $# -lt 2 ]; then
+  echo "No directory arguments provided. Please specify at least one folder representing a chain name."
+  exit 1
+fi
+
+# Determine the number of migrations based on the last argument
+last_argument="${!#}"
+if [[ "$last_argument" =~ ^[0-9]+$ ]]; then
+  num_migrations="$last_argument"
+else
+  num_migrations=1
+fi
+
+# Iterate over each directory argument starting from the second one
+for dir in "${@:2:$#-1}"; do
+  # Check if the directory exists
+  if [ ! -d "$root_folder/$dir" ]; then
+    echo "Directory not found: $root_folder/$dir. Please ensure the folder represents a valid chain name."
+    continue
+  fi
+
+  # Generate the destination filenames
+  for ((i = 1; i <= num_migrations; i++)); do
+    filename="migration_${dir}_${timestamp}_$(printf "%02d" "$i").${extension}"
+    destination="$root_folder/$dir/$filename"
+
+    # Check if the destination file already exists
+    if [ -f "$destination" ]; then
+      echo "Duplicate found: $destination. Skipping."
+      continue
+    fi
+
+    # Copy the template file to the destination
+    cp "$template_file" "$destination"
+    echo "Copied template file to: $destination"
+  done
+done
